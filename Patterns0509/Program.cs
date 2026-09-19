6e8f26fb8edd29a7125e341e03be423c5b92db03
Смолев Даниил Юрьевич <d.smolev@univ.priv>
2026-09-19 09:34:42 +0300

    init

---
+++ Patterns0509/Program.cs
@@ -0,0 +1,22 @@
+﻿using Patterns0509.Generation;
+using Patterns0509.Generation.AbstractFactory;
+using Patterns0509.Generation.FactoryMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns0509
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton.Run();
+            //Prototype.Run();
+            //FactoryMethod.Run();
+            AbstractFactory.Run();
+        }
+    }
+}
